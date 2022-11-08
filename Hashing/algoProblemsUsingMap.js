2f42e8744aa45d3643e0dc6220edb818a8d94062
Rishat Sabitov <rdsabitov777@gmail.com>
2022-11-08 12:05:59 -0500

    additional hashMap problems

--- Hashing/algoProblemsUsingMap.js
+++ Hashing/algoProblemsUsingMap.js
@@ -113,3 +113,198 @@ var countElements = function(arr) {
     })
     return count
 }
+
+//You are given a string s and an integer k. Find the length of the longest substring
+//that contains at most k distinct characters
+
+//For example, given s = "eceba" and k = 2, return 3. 
+//The longest substring with at most 2 distinct characters is "ece".
+
+let findLongestSubstring = (s, k) => { 
+    let counts = new Map()
+    let left = 0, ans = 0
+    for (let right = 0; right<s.length; right++) { 
+        counts.set(s[right], (counts.get(s[right]) || 0) + 1)
+        while(counts.size > k) { 
+            counts.set(s[left], counts.get(s[left]) -1);
+            if(counts.get(s[left])===0) { 
+                counts.delete(s[left]);
+            }
+            left++
+        }
+        ans = Math.max(ans, right-left+1)
+    }
+    return ans
+}
+
+
+//Given a 2D array nums that contains n arrays of distinct integers, return a sorted array
+//containing all the numbers that appear in all n arrays
+
+//For example, given nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]], 
+//return [3, 4]. 3 and 4 are the only numbers that are in all arrays.
+
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+
+var intersection = function(nums) { 
+    let counts = new Map();
+    for (const arr of nums) { 
+        for (const x of arr) { 
+            counts.set(x, (counts.get(x) || 0) + 1)
+        }
+    }
+    let n = nums.length;
+    let ans = [];
+    for(const [key, val] of counts) { 
+        if (val ===n) { 
+            ans.push(key)
+        }
+    }
+    ans.sort((a,b) => a-b);
+    return ans;
+}
+
+
+//Given a string s, determine if all characters have the same frequency.
+
+//For example, given s = "abacbc", return true. All characters appear twice. 
+//Given s = "aaabb", return false. "a" appears 3 times, "b" appears 2 times. 3 != 2.
+
+/**
+ * 
+ * @param {string} s 
+ * @return {boolean}
+ */
+var areOccurencesEqual = function (s) { 
+    let counts = new Map();
+    for(const c of s) { 
+        counts.set(c, (counts.get(c) || 0) + 1)
+    }
+    let frequencies = new Set()
+    for (const val of counts.values()) { 
+        frequencies.add(val)
+    }
+    return frequencies.size===1
+};
+
+
+
+
+//Given an integer array nums and an integer k
+//find the number of subarrays whose sum is equal to k.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+var subarraySum = function(nums,k) { 
+    let counts = new Map();
+    counts.set(0,1);
+    let ans = 0, curr =0;
+    for (const num of nums) { 
+        curr+=num;
+        ans += counts.get(curr-k) || 0;
+        counts.set(curr, (counts.get(curr) || 0) + 1)
+    }
+    return ans;
+}
+
+
+//Notes
+//We use curr to track the prefix sum
+//At any index i, the sum up to i is curr. If there is an index j whose prefix is curr -k, then the sum of the subarray from (j,i) is curr - (curr-k)=k
+//Because the array can have negative numbers, the same prefix can occur multiple times. We use a hashmap counts to track how many 
+//times a prefix has occurred
+//At every index i, the frequency of curr -k is equal to the number of subarrays whose sum is equal to k that end at i. Add it to the answer
+
+
+//Given an array of positive integers nums and an integer k. Find the number of subarrays with exactly k odd numbers in them.
+
+//For example, given nums = [1, 1, 2, 1, 1], k = 3, the answer is 2. The subarrays with 3 odd numbers in them are [1, 1, 2, 1, 1] and [1, 1, 2, 1, 1].
+
+/**
+ * 
+ * @param {number[]} nums 
+ * @param {number} k 
+ * @return {number}
+ */
+var numberOfSubarrays = function(nums, k) { 
+    let counts = new Map();
+    counts.set(0,1)
+    let ans =0, curr =0;
+    for (const num of nums) { 
+        curr += num %2
+        ans +=counts.get(curr-k) ||0;
+        counts.set(curr, (curr.get(curr) || 0) + 1)
+    }
+    return ans;
+}
+
+
+//You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+//Return a list answer of size 2 where:
+
+//answer[0] is a list of all players that have not lost any matches.
+//answer[1] is a list of all players that have lost exactly one match.
+
+//The values in the two lists should be returned in increasing order.
+
+/**
+ * 
+ * @param {number[][]} matches 
+ * @return {number [][]}
+ */
+
+var findWinners = function(matches) { 
+   const formatPlayers ={}
+   for (let i=0; i<matches.length;i++) { 
+       const match = matches[i]
+        if(!formatPlayers[match[1]]) { 
+            formatPlayers[match[1]] = 0;
+        }
+        if(!formatPlayers[match[0]]) { 
+            formatPlayers[match[0]] = 0
+        }
+        formatPlayers[match[1]] ++
+   }
+   const keys = Object.keys(formatPlayers)
+   const noLosses=[]
+   const lostOnce=[]
+   for (let i=0; i<keys.length;i++) { 
+       if(formatPlayers[keys[i]]===0) { 
+           noLosses.push(keys[i])
+       }
+       if(formatPlayers[keys[i]]===1) { 
+           lostOnce.push(keys[i])
+       }
+   }
+   return [noLosses, lostOnce]
+}
+
+//Given an integer array nums, return the largest integer that only occurs once. If no integer occurs once, return -1.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var largestUniqueNumber = function(nums) { 
+   let myMap = new Map();
+    for (const k of nums) { 
+        myMap.set(k, (myMap.get(k) ||0 )+1)
+
+    }
+    let result = -1;
+    for (const [key, val] of myMap) { 
+        if(val===1) { 
+            result = Math.max(result, key)
+        }
+    }
+   return result
+}
